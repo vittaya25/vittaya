c447bb9d612156e86f8e22f90e12ce1efd656a50
vittaya25 <vittayalp@gmail.com>
2021-05-04 19:48:48 +0700

    V2

---
+++ index.js
@@ -0,0 +1,28 @@
+window.addEventListener('scroll',  () => {
+  var scrolly = window.scrollY;
+  console.log(scrolly);
+  const stylebar = document.getElementById("show_bar").style.display;
+  console.log(stylebar);
+  if(scrolly > 260){
+    if(stylebar == "" || stylebar == "none"){
+      document.getElementById("show_bar").style.display = "block";
+      document.getElementById("select_language").style.display = "none";
+    }
+
+  }else if (scrolly < 180 ) {
+    if(stylebar == "block"){
+      document.getElementById("show_bar").style.display = "none";
+      document.getElementById("select_language").style.display = "block";
+    }
+
+  }
+});
+/*   button contact */
+function showcontact() {
+  document.querySelector(".click_show_icon_contact").style.display = "none" ;
+  document.querySelector(".icon_contact").style.display = "block" ;
+}
+function closecontact() {
+  document.querySelector(".click_show_icon_contact").style.display = "block" ;
+  document.querySelector(".icon_contact").style.display = "none" ;
+}
